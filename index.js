86382df79ec9c256ce57a22668d6c438cc734afe
eduwinner <eduardo@winnertecnologia.com.br>
2025-06-14 19:44:20 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+class formaDeBolo {
+  constructor(saborDaMassa, saborDoRecheio) {
+    this.saborDaMassa = saborDaMassa;
+    this.saborDoRecheio = saborDoRecheio;
+  }
+
+  // Método para descrever o bolo
+  escrever() {
+    console.log(`Este bolo tem massa de ${this.saborDaMassa} e recheio de ${this.saborDoRecheio}.`);
+  }
+  assar(){
+    console.log(`Bolo de ${this.saborDaMassa} assando`)
+  }
+}
+let boloFesta = new formaDeBolo("Chocolate", "Nutella");
+boloFesta.escrever();
+boloFesta.assar();
